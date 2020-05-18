7f7f42b42c376a864db85547e8d1ae71a481b379
Formdev <contact@form-dev.fr>
2020-05-18 16:19:47 +0200

    reponse

--- src/Entity/Reponse.php
+++ src/Entity/Reponse.php
@@ -9,11 +9,23 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Reponse
 {
+     /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $Reponse;
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    
     public function getReponse(): ?string
     {
         return $this->Reponse;
